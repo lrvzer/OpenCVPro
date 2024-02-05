b2c78093a414091a6b3b88c37b6b30ea740b4c75
rongwei.li <rongwei.li@jutze.com.cn>
2024-02-05 18:32:36 +0800

    start

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char const *argv[])
+{
+    /**
+     * @brief default： cv::IMREAD_COLOR
+     * cv::IMREAD_GRAYSCALE  灰度图像
+     * cv::IMREAD_ANYCOLOR
+     */
+    cv::Mat src = cv::imread("./images/lena.jpg", cv::IMREAD_ANYCOLOR); // 读取一张图像
+    if (src.empty()) 
+    {
+        std::cout << "picture could not find." <<std::endl;
+        return -1;
+    }
+    /**
+     * cv::WINDOW_AUTOSIZE   适配图像大小，窗口不可改动
+     * cv::WINDOW_FREERATIO  窗口可改动
+     * cv::WINDOW_NORMAL     窗口可改动
+     */
+    cv::namedWindow("Input", cv::WINDOW_NORMAL);
+    cv::imshow("Input", src);
+    cv::waitKey(0); // 阻塞
+    cv::destroyAllWindows(); // 销毁所有窗口
+    return 0;
+}
