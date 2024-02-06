9dbb5f61200e46780d94d22bc6ba1ec5f2244ebe
rongwei.li <rongwei.li@jutze.com.cn>
2024-02-06 13:12:45 +0800

    env

--- main.cpp
+++ main.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-int main(int argc, char const *argv[])
+int main(int argc, char const* argv[])
 {
+    std::string filename;
+    if (argc < 2)
+    {
+        std::cout << "please input image path!" << std::endl;
+        return -1;
+    }
+    filename = argv[1];
     /**
      * cv::IMREAD_COLOR
      * cv::IMREAD_GRAYSCALE  灰度图像
      * cv::IMREAD_ANYCOLOR
      */
-    cv::Mat src = cv::imread("../images/lena.jpg", cv::IMREAD_ANYCOLOR); // 读取一张图像
-    if (src.empty()) 
+    // "../images/lena.jpg"
+    cv::Mat src = cv::imread(filename, cv::IMREAD_ANYCOLOR); // 读取一张图像
+    if (src.empty())
     {
-        std::cout << "picture could not find." <<std::endl;
+        std::cout << "picture could not find." << std::endl;
         return -1;
     }
     /**
@@ -21,7 +29,7 @@ int main(int argc, char const *argv[])
      */
     cv::namedWindow("Input", cv::WINDOW_NORMAL);
     cv::imshow("Input", src);
-    cv::waitKey(0); // 阻塞
+    cv::waitKey(0);          // 阻塞
     cv::destroyAllWindows(); // 销毁所有窗口
     return 0;
 }
