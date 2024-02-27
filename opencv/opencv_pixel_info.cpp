6cc881bcfac9fa3e7cd0769be475f8bde9c55a55
rongwei.li <rongwei.li@jutze.com.cn>
2024-02-27 11:24:09 +0800

    Pixel information statistics

--- opencv/opencv_pixel_info.cpp
+++ opencv/opencv_pixel_info.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <vector>
 
 /***
  * 图像最大与最小值
@@ -15,7 +16,7 @@ int main(int argc, char const* argv[])
         return -1;
     }
     filename = argv[1];
-    cv::Mat src = cv::imread(filename, cv::IMREAD_ANYCOLOR); // 读取一张图像
+    cv::Mat src = cv::imread(filename, cv::IMREAD_UNCHANGED); // 读取一张图像
     if (src.empty())
     {
         std::cout << "picture could not find." << std::endl;
@@ -25,14 +26,62 @@ int main(int argc, char const* argv[])
     cv::Mat dst;
     int rows = src.rows;
     int cols = src.cols;
-    int dims = src.channels();
+    int ch = src.channels();
 
-    printf("width: %d, height: %d, dims: %d\n", cols, rows, dims);
+    printf("width: %d, height: %d, channels: %d\n", cols, rows, ch);
 
+    // 最大值最小值
+    cv::Mat image_re = src.reshape(1);
     double minValue, maxValue;
     cv::Point minLocPoint, maxLocPoint;
-    cv::minMaxLoc(src, &minValue, &maxValue, &minLocPoint, &maxLocPoint);
+    /**
+     * Finds the global minimum and maximum in an array.
+     * The function cv::minMaxLoc finds the minimum and maximum element values and their positions.
+     * The extremums are searched across the whole array or, if mask is not an empty array, in the specified array region.
+     * The function do not work with multi-channel arrays.
+     *
+     * Notes:
+     * If you need to find minimum or maximum elements across all the channels, use Mat::reshape first to reinterpret the array as single-channel.
+     * Or you may extract the particular channel using either extractImageCOI, or mixChannels, or split.
+     */
+    cv::minMaxLoc(image_re, &minValue, &maxValue, &minLocPoint, &maxLocPoint); // 不支持多通道数组
     printf("Min: %.2f, Max: %.2f\n", minValue, maxValue);
+
+    // 均值方差
+    cv::Scalar s = cv::mean(src);
+    printf("mean b: %.2f, g: %.2f, r: %.2f\n", s[0], s[1], s[2]);
+
+    cv::Mat mm, mstd;
+    // 均值和方差
+    cv::meanStdDev(src, mm, mstd);
+    printf("rows: %d, cols: %d\n", mstd.rows, mstd.cols);
+    printf("mean  : %.2f, %.2f, %.2f\n", mm.at<double>(0, 0), mm.at<double>(1, 0), mm.at<double>(2, 0));
+    printf("stddev: %.2f, %.2f, %.2f\n", mstd.at<double>(0, 0), mstd.at<double>(1, 0), mstd.at<double>(2, 0));
+
+    // 像素值统计
+    std::vector<int> histB(256);
+    std::vector<int> histG(256);
+    std::vector<int> histR(256);
+    std::vector<int> hist(256);
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            if (ch == 3)
+            {
+                cv::Vec3b pv = src.at<cv::Vec3b>(row, col);
+                histB[pv[0]]++;
+                histG[pv[1]]++;
+                histR[pv[2]]++;
+            }
+
+            if (ch == 1)
+            {
+                int pv = src.at<uchar>(row, col);
+                hist[pv]++;
+            }
+        }
+    }
 
     cv::namedWindow("Input", cv::WINDOW_NORMAL);
     cv::imshow("Input", src);
